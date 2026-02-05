9f9de7ce19cf46b63b3237be840e740546fc3fb5
HeuiEun Choi <52269210+nana1243@users.noreply.github.com>
2026-02-05 10:33:37 +0900

    fea) [2/5] 웰캄 그리팅 화면에 필요한 css 추가

--- src/components/greeting/index.tsx
+++ src/components/greeting/index.tsx
@@ -1,16 +1,19 @@
-import styles from './index.module.css'
+import styles from './index.module.css';
+import weddingBackground from '../../assets/image/wedding_background.jpg';
 
 const Greeting = () => {
     return (
         <div className={styles.container}>
             <div className={styles.imageAreaGuide}>
+                <img src={weddingBackground} alt="wedding background" className={styles.backgroundImage}/>
+
                 <div className={styles.nameOverlay}>
                     <span>이준서</span>
                     <span>김은재</span>
                 </div>
                 <div className={styles.informationOverlay}>
                     <p>
-                    2025.10.18 SAT PM 02:30
+                        2025.10.18 SAT PM 02:30
                     </p>
                     <p>
                         메종 드 프리미어 그랜드홀
@@ -18,7 +21,6 @@ const Greeting = () => {
                 </div>
             </div>
         </div>
-
     );
 };
 
