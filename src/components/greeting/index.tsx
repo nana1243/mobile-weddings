032e04f9aac871377018138ca690b9501e56f487
HeuiEun Choi <52269210+nana1243@users.noreply.github.com>
2026-02-05 10:22:14 +0900

    fea) [2/5] 웰캄 그리팅 화면에 필요한 css 추가

--- src/components/greeting/index.tsx
+++ src/components/greeting/index.tsx
@@ -2,12 +2,24 @@ import styles from './index.module.css'
 
 const Greeting = () => {
     return (
-        <>
-            <div className={styles.container}>
-                <h1>Hello!</h1>
+        <div className={styles.container}>
+            <div className={styles.imageAreaGuide}>
+                <div className={styles.nameOverlay}>
+                    <span>이준서</span>
+                    <span>김은재</span>
+                </div>
+                <div className={styles.informationOverlay}>
+                    <p>
+                    2025.10.18 SAT PM 02:30
+                    </p>
+                    <p>
+                        메종 드 프리미어 그랜드홀
+                    </p>
+                </div>
             </div>
-        </>
-    )
-}
+        </div>
+
+    );
+};
 
 export default Greeting;
