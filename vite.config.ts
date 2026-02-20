3c4abbd4a0008f3fd3589c7e4704737c62662643
HeuiEun Choi <52269210+nana1243@users.noreply.github.com>
2026-02-20 11:42:29 +0900

    fea) [2/20] 배포시 설정

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import path from 'path';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base : 'mobile-weddings',
   resolve: {
     alias: [
       { find: '@', replacement: path.resolve(__dirname, 'src') }
